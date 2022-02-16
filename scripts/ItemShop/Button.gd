608c841813012d7708202c12325f26884df48211
ShadowFungi <shadowfungi.meddr@aleeas.com>
2022-02-16 13:12:29 -0700

    base for item shop
    a basic start for an item shop, a start script

--- scripts/ItemShop/Button.gd
+++ scripts/ItemShop/Button.gd
@@ -3,4 +3,6 @@ extends Button
 
 
 func _on_x2CoinButton_pressed():
-	pass # Replace with function body.
+	var shop_items = Global.shop_items.new()
+	shop_items.double_coins = true
+	disabled = true
