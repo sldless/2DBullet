1b4e9dd88c7d40d92c82e9974f6a1b445a3b426e
sldless <67599596+sldless@users.noreply.github.com>
2022-02-15 21:25:03 -0500

    Update home.gd
    removed unneeded variable added a quit and item shop button

--- scripts/Menu/home.gd
+++ scripts/Menu/home.gd
@@ -1,8 +1,13 @@
 extends Button
 
-
-var scene = "res://scripts/main.tscn"
-
-func _pressed() -> void:
-	get_tree().change_scene(scene)
+func _on_PlayButton_pressed() -> void:
+	get_tree().change_scene("res://scripts/main.tscn")
 	
+
+
+func _on_icButton_pressed():
+	pass # Replace with function body.
+	
+
+func _on_quitButton_pressed():
+	get_tree().quit()
