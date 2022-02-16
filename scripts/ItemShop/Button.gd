6c0b5677f8cc7a669446424c6f9b546981341bae
sldless <67599596+sldless@users.noreply.github.com>
2022-02-16 15:04:26 -0500

    Added a back button for item shop

--- scripts/ItemShop/Button.gd
+++ scripts/ItemShop/Button.gd
@@ -2,5 +2,9 @@ extends Button
 
 
 
-func _on_x2CoinButton_pressed():
+func _on_x2CoinButton_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_backButton_pressed() -> void:
+	get_tree().change_scene("res://scripts/Menu/Menu.tscn")
