7a261fad414716a42b76ac1706de96f9ed9b3df0
sldless <67599596+sldless@users.noreply.github.com>
2022-02-16 17:07:43 -0500

    removed disabled

--- scripts/ItemShop/ShopButtons.gd
+++ scripts/ItemShop/ShopButtons.gd
@@ -1,11 +1,10 @@
-extends Button
+extends Control
 
 
 
 func _on_x2CoinButton_pressed():
 	var shop_items = Global.shop_items.new()
 	shop_items.double_coins = true
-	disabled = true
 
 
 func _on_backButton_pressed() -> void:
