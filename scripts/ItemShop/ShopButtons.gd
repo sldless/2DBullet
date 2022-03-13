39bf5a3386fd54846c37f89c5356f56cfbdf675e
sldless <67599596+sldless@users.noreply.github.com>
2022-03-13 00:36:36 -0500

    Last commit maybe

---
+++ scripts/ItemShop/ShopButtons.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_x2CoinButton_pressed():
+	var shop_items = Global.shop_items.new()
+	shop_items.double_coins = true
+
+
+func _on_backButton_pressed() -> void:
+	get_tree().change_scene("res://scripts/Menu/Menu.tscn")
